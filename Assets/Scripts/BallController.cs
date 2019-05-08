48a65a95f863226e1a78c0e5d522486c9c7862cc
Tre Shine <t00184855@students.ittralee.ie>
2019-05-08 16:04:20 +0100

    initial commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    
+    bool started;
+    bool gameOver;
+    Rigidbody2D rb; // set to Kinematic type in Unity
+    public float upForce; // 200 works well
+    public float rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> (); 
+        started = false; 
+        gameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Game not started
+        if (!started) {
+            /* GetMouseButton works for single taps as well
+               Input.GetMouseButtonDown (0) is the first touch */
+            if (Input.GetMouseButtonDown (0)) {
+                started = true;
+                rb.isKinematic = false; // ball is not moving yet
+                //GameManager.instance.GameStart ();
+            }
+        } 
+        // Game is started
+        else if(started && !gameOver){
+            transform.Rotate (0, 0, rotation);
+            // Game is started and user is tapping
+            if (Input.GetMouseButtonDown (0)) {
+                // set initial ball velocity to 0 to allow enough upward motion
+                rb.velocity = Vector2.zero;
+                // Tap will only add force to y direction 
+                rb.AddForce (new Vector2 (0, upForce));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PipeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    public float speed; //how fast pipes move
+    public float upDownSpeed; // how fast pipes move up and down
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        MovePipe (); // starts moving the pipes
+        
+        // SwitchUpDown invoked after 0.1 seconds -> repeated every 1 seconds
+        InvokeRepeating ("SwitchUpDown", 0.1f, 1f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void MovePipe()
+    { 
+        // -speed will move pipes toward player
+        rb.velocity = new Vector2 (-speed, upDownSpeed);
+    }
+    void SwitchUpDown(){
+        upDownSpeed = -upDownSpeed;
+        rb.velocity = new Vector2 (speed, upDownSpeed);
+    }
+}
